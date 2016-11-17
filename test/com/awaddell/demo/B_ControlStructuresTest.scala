b32af20e36ea54c51fb0ea36427a7b2af361a250
Aaron Bourne <contact@aaronbourne.co.uk>
2016-11-17 20:03:19 +0000

    Add control structures examples

--- test/com/awaddell/demo/B_ControlStructuresTest.scala
+++ test/com/awaddell/demo/B_ControlStructuresTest.scala
@@ -1,12 +1,63 @@
 package com.awaddell.demo
 
+import org.scalatest.FunSuite
 
-class B_ControlStructuresTest {
 
-  // TODO: If
+/**
+  * Demonstrated Scala control structures
+  */
+class B_ControlStructuresTest extends FunSuite {
 
-  // TODO: While
+  /**
+    * If statements
+    *
+    * The only differences in Scala is that an if is a function so it returns a value
+    */
+  test("Demonstrate if") {
+    val result = if (true) {
+      "I'm true!"
+    } else {
+      "I'm false!"
+    }
 
-  // TODO: For
+    println(result)
+  }
+
+
+  /**
+    * While loops
+    *
+    * The same as Java!
+    */
+  test("Demonstrate while") {
+
+    var count = 0
+    val max = 10
+
+    while (count <= max) {
+      println(count)
+      count += 1
+    }
+  }
+
+
+  /**
+    * For is a bit different in Scala
+    *
+    * In Java you might see this:
+    *
+    * for(int i = 0; i <= 10; i++){
+    *     System.out.println(i);
+    * }
+    *
+    * Yeah, there's no equivalent to that in Scala. Instead you
+    * pass a Range into the for loop.
+    */
+  test("Demonstrate for") {
+
+    for (n <- 1.to(10)) {
+      println(n)
+    }
+  }
 
 }
